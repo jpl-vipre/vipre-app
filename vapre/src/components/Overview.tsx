b64ca22a59d20e9351a855ef85e416d727d8edd8
RyanStonebraker <ryanastonebraker@gmail.com>
2022-04-01 13:34:12 -0700

    Adds overview placeholder elements

--- vapre/src/components/Overview.tsx
+++ vapre/src/components/Overview.tsx
@@ -3,15 +3,57 @@ import { FC } from "react";
 import "../scss/Overview.scss";
 
 const Overview: FC = () => {
-  // const overviewFields = [
-  //   {
-  //     "field": "launchDate",
-  //     "label": "Launch Date",
-  //     "units": "Years Past 2000",
-  //     "value": 34.63
-  //   }
-  // ]
-  
+  const overviewFields = [
+    {
+      display_name: "Launch Date",
+      field_name: "t_launch",
+      units: "Years Past 2000",
+      value: 34.63,
+    },
+    {
+      display_name: "Arrival Date",
+      field_name: "t_arr",
+      units: "Years Past 2000",
+      value: 42.8,
+    },
+    {
+      display_name: "Arrival Mass",
+      field_name: "m_arr",
+      units: "Years Past 2000",
+      value: 200.2,
+    },
+    {
+      display_name: "Arrival V ∞",
+      field_name: "v_inf_arr_x",
+      units: "Vector X",
+      value: 165.32,
+    },
+    {
+      display_name: "Arrival V ∞",
+      field_name: "v_inf_arr_y",
+      units: "Vector Y",
+      value: 84.53,
+    },
+    {
+      display_name: "Arrival V ∞",
+      field_name: "v_inf_arr_z",
+      units: "Vector Z",
+      value: 932.3,
+    },
+    {
+      display_name: "Launch C3",
+      field_name: "c3",
+      units: "km2/s2",
+      value: 12.54,
+    },
+    {
+      display_name: "Total Cruise ΔV",
+      field_name: "dv_total",
+      units: "km/s",
+      value: 67.24,
+    },
+  ];
+
   return (
     <div className="overview-container">
       <div className="top" style={{ display: "flex" }}>
@@ -19,17 +61,15 @@ const Overview: FC = () => {
         <div style={{ marginLeft: "auto" }}>Trajectory ID</div>
       </div>
       <div className="overview-list">
-        {/* {
-          overviewFields.map((overviewField) => {
-            return (
-              <div key={overviewField.field} className="overview-field">
-                <h2>{overviewField.value}</h2>
-                <div>{overviewField.label}</div>
-                <div>{overviewField.units}</div>
-              </div>
-            )
-          })
-        } */}
+        {overviewFields.map((overviewField) => {
+          return (
+            <div key={overviewField.field_name} className="overview-field">
+              <h2>{overviewField.value}</h2>
+              <h5>{overviewField.display_name}</h5>
+              <h6>{overviewField.units}</h6>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- vapre/src/index.tsx
+++ vapre/src/index.tsx
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import "material-design-icons/iconfont/material-icons.css";
+import "typeface-roboto/index.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
