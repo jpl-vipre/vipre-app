651abea28e10ddcb4276d0cb8f03cac362ac487c
RyanStonebraker <ryanastonebraker@gmail.com>
2022-04-01 17:31:40 -0700

    Hooks up AllFieldsSelect to API

--- vapre/src/components/AllFieldsSelect.tsx
+++ vapre/src/components/AllFieldsSelect.tsx
@@ -1,5 +1,6 @@
-import { FC } from "react";
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { FC, useMemo } from "react";
+import { FormControl, InputLabel, MenuItem, Select, ListSubheader } from "@mui/material";
+import useStore from "../utils/store";
 
 interface AllFieldsSelectProps {
   value?: string;
@@ -8,9 +9,20 @@ interface AllFieldsSelectProps {
   options?: string[];
 }
 const AllFieldsSelect: FC<AllFieldsSelectProps> = ({ value, label, onChange, options }) => {
-  if (options === undefined) {
-    options = ["a", "b", "c"];
-  }
+  const entryFields = useStore((state) => state.entryFields);
+  const trajectoryFields = useStore((state) => state.trajectoryFields);
+
+  const trajectoryOptions = useMemo(() => {
+    if (options && Array.isArray(options)) {
+      return options;
+    }
+
+    return trajectoryFields.map((field) => field.field_name);
+  }, [options, trajectoryFields]);
+
+  const entryOptions = useMemo(() => {
+    return entryFields.map((field) => field.field_name);
+  }, [entryFields]);
 
   return (
     <FormControl fullWidth style={{ margin: "5px" }}>
@@ -20,15 +32,24 @@ const AllFieldsSelect: FC<AllFieldsSelectProps> = ({ value, label, onChange, opt
         variant="standard"
         style={{ textAlign: "left", paddingLeft: "5px" }}
         labelId={`${label.replace(/ /g, "-").toLowerCase()}-graph-type-label`}
-        value={value && options.includes(value) ? value : ""}
+        value={value && (trajectoryOptions.includes(value) || entryOptions.includes(value)) ? value : ""}
         label={label}
         onChange={(evt: any) => {
           if (onChange) onChange(evt.target.value as string);
         }}
       >
-        {options.map((option: any) => {
-          return <MenuItem value={option}>{option}</MenuItem>;
-        })}
+        {!options && <ListSubheader>Trajectory Fields</ListSubheader>}
+        {trajectoryOptions.map((option: any) => (
+          <MenuItem value={option}>{option}</MenuItem>
+        ))}
+        {!options && (
+          <>
+            <ListSubheader>Entry Fields</ListSubheader>
+            {entryOptions.map((option: any) => (
+              <MenuItem value={option}>{option}</MenuItem>
+            ))}
+          </>
+        )}
       </Select>
     </FormControl>
   );
