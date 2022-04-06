eb9eeaa49e222803d2e8d7f5a3a59bd70a08ac81
RyanStonebraker <ryanastonebraker@gmail.com>
2022-04-06 09:46:35 -0700

    Adds traj id and flyby seq placeholders

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -9,7 +9,7 @@ const exampleValues = [34.63, 42.8, 200.2, 165.32, 84.53, 932.41, 12.54, 67.24];
 const nameRemapping: Record<string, Partial<FilterField>> = {
   t_launch: { display_name: "Launch Date", units: "Years Past 2000" },
   t_arr: { display_name: "Arrival Date", units: "Years Past 2000" },
-  m_arr: { display_name: "Arrival Mass", units: "At arrival, kg" },
+  // m_arr: { display_name: "Arrival Mass", units: "At arrival, kg" },
   v_inf_arr_x: { display_name: "Arrival V ∞", units: "Vector X" },
   v_inf_arr_y: { display_name: "Arrival V ∞", units: "Vector Y" },
   v_inf_arr_z: { display_name: "Arrival V ∞", units: "Vector Z" },
@@ -21,10 +21,12 @@ const Overview: FC = () => {
   const trajectoryFields = useStore((state) => state.trajectoryFields);
 
   const overviewFields = useMemo(() => {
-    return trajectoryFields.map((field, i) => {
-      const remappedField: FilterField = {
+    let fields: Partial<FilterField>[] = trajectoryFields && trajectoryFields.length > 0 ? trajectoryFields : Object.entries(nameRemapping).map(([key, value]) => ({ field_name: key, ...value }));
+    console.log(fields)
+    return fields.map((field, i) => {
+      const remappedField: Partial<FilterField> = {
         ...field,
-        ...(nameRemapping[field.field_name] || {}),
+        ...(nameRemapping[field.field_name!] || {}),
       };
       return {
         display_name: remappedField.display_name,
@@ -37,9 +39,10 @@ const Overview: FC = () => {
 
   return (
     <div className="overview-container">
-      <div className="top" style={{ display: "flex" }}>
+      <div className="top" style={{ display: "flex", marginTop: "5px" }}>
         <h1>Overview</h1>
-        <div style={{ marginLeft: "auto" }}>Trajectory ID</div>
+        <div style={{ marginLeft: "auto", background: "black", padding: "5px", borderRadius: "5px" }}><b>Trajectory ID:</b> 12345</div>
+        <div style={{ marginLeft: "15px", marginRight: "5px", background: "black", padding: "5px", borderRadius: "5px" }}><b>Flyby Sequence:</b> 123-456-789</div>
       </div>
       <div className="overview-list">
         {overviewFields.map((overviewField) => {
