d46063eef4d79f6f2a1635c776b3b12200df612c
RyanStonebraker <ryanastonebraker@gmail.com>
2022-05-05 13:36:50 -0700

    Adds react-globe example with sample data

--- src/components/Visualizations/Globe.tsx
+++ src/components/Visualizations/Globe.tsx
@@ -1,13 +1,23 @@
-import { FC, useMemo, useState } from "react";
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Cell, ResponsiveContainer } from "recharts";
+import { FC } from "react";
+import {  ResponsiveContainer } from "recharts";
 import { scaleSequential } from "d3-scale";
 import { interpolateSpectral } from "d3-scale-chromatic";
 
-import ColorScale from "./ColorScale";
+import ReactGlobe from "react-globe.gl";
+
 
 import "../../scss/Globe.scss";
 
-const colors = scaleSequential(interpolateSpectral);
+// const colors = scaleSequential(interpolateSpectral);
+
+const N = 100;
+// @ts-ignore
+  const gData = [...Array(N).keys()].map(() => ({
+    lat: (Math.random() - 0.5) * 180,
+    lng: (Math.random() - 0.5) * 360,
+    size: Math.random() / 3,
+    color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
+  }));
 
 interface GlobeProps {
   id: string;
@@ -23,12 +33,23 @@ const Globe: FC<GlobeProps> = ({ data, xField, xUnits, yField, yUnits, colorFiel
 
   return (
     <div
-      style={{ display: "flex", width: "100%", height: "100%", maxHeight: "calc(100% - 15px)", alignItems: "center" }}
+      style={{ display: "flex", width: "100%", height: "100%", maxHeight: "calc(100% - 15px)", alignItems: "center", background: "black" }}
       id={id}
     >
-      <div style={{ width: "100%", height: "100%" }} className="globe-container">
-        Globe
+      <ResponsiveContainer>
+      <div style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center" }} className="globe-container">
+        <ReactGlobe
+          globeImageUrl={"//unpkg.com/three-globe/example/img/earth-day.jpg"}
+          pointsData={gData}
+          pointAltitude="size"
+          pointColor="color"
+          backgroundColor="black"
+          // @ts-ignore
+          width={450}
+          height={300}
+        />
       </div>
+      </ResponsiveContainer>
     </div>
   );
 };
--- src/components/Visualizations/Visualization.tsx
+++ src/components/Visualizations/Visualization.tsx
@@ -5,15 +5,6 @@ import Globe from "./Globe";
 
 import "../../scss/Visualization.scss";
 
-// const TEST_DATA = [
-//   { a: 1, b: 2, c: 1 },
-//   { a: 2, b: 3, c: 10 },
-//   { a: 3, b: 4, c: 2 },
-//   { a: 4, b: 5, c: 10 },
-//   { a: 5, b: 6, c: 5 },
-//   { a: 6, b: 7, c: 7 },
-// ];
-
 const TEST_DATA =[
     {
         "id": 1,
@@ -76,6 +67,7 @@ const Visualization: FC<VisualizationProps> = ({ config, id }) => {
         padding: "5px",
         border: "1px solid #a1a1b5",
         borderRadius: "5px",
+        background: config.type === "globe" ? "black" : "#232E44"
       }}
       id={id}
     >
